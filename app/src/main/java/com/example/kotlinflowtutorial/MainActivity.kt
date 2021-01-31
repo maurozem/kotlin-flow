eeb7bbf823714809c07e2f6684a67789247317e5
Douglas Motta <tibeca@gmail.com>
2021-01-31 15:00:00 +0000

    Adjust skeleton

--- app/src/main/java/com/example/kotlinflowtutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlinflowtutorial/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.kotlinflowtutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.activity.viewModels
 import com.example.kotlinflowtutorial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
--- app/src/main/java/com/example/kotlinflowtutorial/MainViewModel.kt
+++ app/src/main/java/com/example/kotlinflowtutorial/MainViewModel.kt
@@ -1,13 +1,11 @@
 package com.example.kotlinflowtutorial
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
 
-    private val _languagesData = MutableLiveData<String>()
-    val languagesData: LiveData<String> get() = _languagesData
+    lateinit var languagesData: LiveData<String>
 
     fun getLanguages() {
 
